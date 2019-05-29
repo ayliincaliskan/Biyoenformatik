6995c75a22fb399c9f3533c96a61c4cb96383cc5
ayliincaliskan <aylin.03975@gmail.com>
2019-05-30 02:56:54 +0300

    Add files via upload

---
+++ bio/odev3/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace odev3
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.match = new System.Windows.Forms.TextBox();
+			this.mismatch = new System.Windows.Forms.TextBox();
+			this.gap = new System.Windows.Forms.TextBox();
+			this.checkbox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(26, 35);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(158, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(26, 72);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(158, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// match
+			// 
+			this.match.Location = new System.Drawing.Point(231, 57);
+			this.match.Name = "match";
+			this.match.Size = new System.Drawing.Size(53, 20);
+			this.match.TabIndex = 2;
+			// 
+			// mismatch
+			// 
+			this.mismatch.Location = new System.Drawing.Point(311, 57);
+			this.mismatch.Name = "mismatch";
+			this.mismatch.Size = new System.Drawing.Size(53, 20);
+			this.mismatch.TabIndex = 3;
+			// 
+			// gap
+			// 
+			this.gap.Location = new System.Drawing.Point(392, 57);
+			this.gap.Name = "gap";
+			this.gap.Size = new System.Drawing.Size(53, 20);
+			this.gap.TabIndex = 4;
+			// 
+			// checkbox1
+			// 
+			this.checkbox1.AutoSize = true;
+			this.checkbox1.Location = new System.Drawing.Point(231, 101);
+			this.checkbox1.Name = "checkbox1";
+			this.checkbox1.Size = new System.Drawing.Size(45, 17);
+			this.checkbox1.TabIndex = 5;
+			this.checkbox1.Text = "NW";
+			this.checkbox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(320, 101);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(44, 17);
+			this.checkBox2.TabIndex = 6;
+			this.checkBox2.Text = "SW";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(228, 28);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(37, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Match";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(308, 28);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 13);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Mismatch";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(389, 28);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(29, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "GAP";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.Location = new System.Drawing.Point(479, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(71, 57);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Gonder";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(45, 140);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(685, 365);
+			this.panel1.TabIndex = 11;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(647, 22);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(0, 13);
+			this.label5.TabIndex = 13;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(584, 22);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(44, 13);
+			this.label6.TabIndex = 14;
+			this.label6.Text = "Score : ";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(587, 57);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(105, 20);
+			this.textBox3.TabIndex = 16;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(587, 83);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(105, 20);
+			this.textBox4.TabIndex = 17;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(779, 517);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkbox1);
+			this.Controls.Add(this.gap);
+			this.Controls.Add(this.mismatch);
+			this.Controls.Add(this.match);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox match;
+		private System.Windows.Forms.TextBox mismatch;
+		private System.Windows.Forms.TextBox gap;
+		private System.Windows.Forms.CheckBox checkbox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+	}
+}
+
---
+++ bio/odev3/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox3.Clear();
+			textBox4.Clear();
+
+			int match1 = Convert.ToInt32(match.Text);
+			int mismatch1 = Convert.ToInt32(mismatch.Text);
+			int gap1 = Convert.ToInt32(gap.Text);
+
+			if (checkbox1.Checked)
+			{
+				textBox3.Clear();
+				textBox4.Clear();
+				panel1.Controls.Clear();
+				string dizi1 = textBox1.Text;
+				string dizi2 = textBox2.Text;
+				int dizi1_length = dizi1.Length;
+				int dizi2_length = dizi2.Length;
+
+				char[] d_dizi1 = new char[dizi1_length + 2];
+				char[] d_dizi2 = new char[dizi2_length + 2];
+
+				d_dizi1 = dizi1.ToCharArray();
+				d_dizi2 = dizi2.ToCharArray();
+
+				string[,] noktaTablosu = new string[dizi2_length + 2, dizi1_length + 2];
+
+				for (int i = 0; i < dizi2_length; i++)
+				{
+					noktaTablosu[1, 1] = gap1.ToString();
+
+					noktaTablosu[i + 2, 0] = d_dizi2[i].ToString();
+					noktaTablosu[i + 2, 1] = gap1.ToString();
+					for (int j = 0; j < dizi1_length; j++)
+					{
+						noktaTablosu[0, j + 2] = d_dizi1[j].ToString();
+						noktaTablosu[1, j + 2] = gap1.ToString();
+					}
+				}
+				/////////////////////////////////SAYILARI DİZDİM
+
+				for (int j = 0; j < dizi2_length; j++)
+				{
+					for (int i = 0; i < dizi1_length; i++)
+					{
+						if (noktaTablosu[j + 2, 0] == noktaTablosu[0, i + 2])
+						{
+
+							int v = Convert.ToInt32(noktaTablosu[j + 1, i + 2]);
+							int m = Convert.ToInt32(noktaTablosu[j + 2, i + 1]);
+							int n = Convert.ToInt32(noktaTablosu[j + 1, i + 1]);
+
+							int a = Math.Max(v + gap1, m + gap1);
+							int b = Math.Max(a, n + match1);
+							noktaTablosu[j + 2, i + 2] = b.ToString();
+						}
+						else
+						{
+							int v = Convert.ToInt32(noktaTablosu[j + 1, i + 2]);
+							int m = Convert.ToInt32(noktaTablosu[j + 2, i + 1]);
+							int n = Convert.ToInt32(noktaTablosu[j + 1, i + 1]);
+
+							int a = Math.Max(v + gap1, m + gap1);
+							int b = Math.Max(a, n + mismatch1);
+							noktaTablosu[j + 2, i + 2] = b.ToString();
+						}
+
+					}
+				}
+
+				///////////////PANELİ OLUŞTURDUM
+				for (int i = 0; i <= dizi2_length + 1; i++)
+				{
+					for (int j = 0; j <= dizi1_length + 1; j++)
+					{
+						Label lbl = new Label();
+						lbl.BorderStyle = BorderStyle.Fixed3D;
+						lbl.BackColor = Color.White;
+						lbl.Text = Convert.ToString(noktaTablosu[i, j]);
+						lbl.Size = new Size(25, 25);
+						lbl.Location = new Point(25 * j, 25 * i);
+						panel1.Controls.Add(lbl);
+
+					}
+				}
+				/////////////////////////EN BÜYÜK SAYIYI BULDUM
+				int u;
+				int max = Convert.ToInt32(noktaTablosu[dizi2_length + 1, 1]);
+				for (u = 2; u < dizi1_length + 1; u++)
+				{
+
+					if (Convert.ToInt32(noktaTablosu[dizi2_length + 1, u]) > max)
+					{
+						max = Convert.ToInt32(noktaTablosu[dizi2_length + 1, u]);
+						//z = u;
+					}
+					else continue;
+				}
+
+				int max2 = 1;
+				int x = dizi2_length + 1;
+				int y = u;
+				int sayac1 = 0;
+				int sayac2 = 0;
+				int sayac3 = 0;
+				int sayac4 = 0;
+				int sayac5 = 0;
+				int sayac6 = 0;
+				int sayac7 = 0;
+				int sayac8 = 0;
+
+				if (noktaTablosu[2, 0] == noktaTablosu[0, 2])
+					sayac7++;
+				if (noktaTablosu[x, 0] == noktaTablosu[0, y])
+				{
+					sayac8++;
+					textBox3.SelectedText = noktaTablosu[x , 0];
+					textBox4.SelectedText = noktaTablosu[0, y];
+
+				}
+
+				while (max2 != 0)
+				{
+
+
+					if ((Convert.ToInt32(noktaTablosu[x, y - 1]) > Convert.ToInt32(noktaTablosu[x - 1, y])) && (Convert.ToInt32(noktaTablosu[x, y - 1]) > Convert.ToInt32(noktaTablosu[x - 1, y - 1])))
+					{
+						max2 = 0;
+						max2 = Convert.ToInt32(noktaTablosu[x, y - 1]);
+						if (noktaTablosu[x, 0] == noktaTablosu[0, y - 1])
+						{
+							sayac4++;
+							
+							textBox3.SelectedText = noktaTablosu[x, 0];
+							textBox4.SelectedText = noktaTablosu[0, y - 1];
+						}
+						x = x;
+						y = y - 1;
+						sayac1 += max2;
+
+					}
+					if ((Convert.ToInt32(noktaTablosu[x - 1, y]) > Convert.ToInt32(noktaTablosu[x, y - 1])) && (Convert.ToInt32(noktaTablosu[x - 1, y]) > Convert.ToInt32(noktaTablosu[x - 1, y - 1])))
+					{
+						max2 = 0;
+						max2 = Convert.ToInt32(noktaTablosu[x - 1, y]);
+						if (noktaTablosu[x - 1, 0] == noktaTablosu[0, y])
+						{
+							sayac5++;
+
+							textBox3.SelectedText = noktaTablosu[x - 1, 0];
+							textBox4.SelectedText = noktaTablosu[0, y];
+							
+						}
+						x = x - 1;
+						y = y;
+						sayac2 += max2;
+
+					}
+					if ((Convert.ToInt32(noktaTablosu[x - 1, y - 1]) >= Convert.ToInt32(noktaTablosu[x, y - 1])) && (Convert.ToInt32(noktaTablosu[x - 1, y - 1]) >= Convert.ToInt32(noktaTablosu[x - 1, y])))
+					{
+						max2 = 0;
+						max2 = Convert.ToInt32(noktaTablosu[x - 1, y - 1]);
+						if (noktaTablosu[x - 1, 0] == noktaTablosu[0, y - 1])
+							sayac6++;
+						//if (noktaTablosu[2, 0] == noktaTablosu[0, 2])
+						//	sayac8++;
+						x = x - 1;
+						y = y - 1;
+						sayac3 += max2;
+
+
+					}
+				}
+
+				string kelime = textBox3.Text;
+				string terskelime = "";
+				foreach (char harf in kelime)
+				{
+					terskelime = harf.ToString() + terskelime;
+				}
+				textBox3.Text = terskelime;
+
+				string kelime2 = textBox4.Text;
+				string terskelime2 = "";
+				foreach (char harf in kelime2)
+				{
+					terskelime2 = harf.ToString() + terskelime2;
+				}
+				textBox4.Text = terskelime2;
+
+				//label4.Text = (sayac1 + sayac2 + sayac3).ToString();
+
+				label5.Text = (sayac6 + sayac7 + sayac8).ToString();
+
+			}
+
+
+
+
+			//////////////////////
+
+			if (checkBox2.Checked)
+			{
+				textBox3.Clear();
+				textBox4.Clear();
+
+				panel1.Controls.Clear();
+				string dizi1 = textBox1.Text;
+				string dizi2 = textBox2.Text;
+				int dizi1_length = dizi1.Length;
+				int dizi2_length = dizi2.Length;
+
+				char[] d_dizi1 = new char[dizi1_length + 2];
+				char[] d_dizi2 = new char[dizi2_length + 2];
+
+				d_dizi1 = dizi1.ToCharArray();
+				d_dizi2 = dizi2.ToCharArray();
+
+				string[,] noktaTablosu = new string[dizi2_length + 2, dizi1_length + 2];
+
+				for (int i = 0; i < dizi2_length; i++)
+				{
+					if (gap1 <= 0)
+					{
+						noktaTablosu[1, 1] = 0.ToString();
+						noktaTablosu[i + 2, 1] = 0.ToString();
+					}
+					else
+					{
+						noktaTablosu[1, 1] = gap1.ToString();
+						noktaTablosu[i + 2, 1] = gap1.ToString();
+					}
+					noktaTablosu[i + 2, 0] = d_dizi2[i].ToString();
+
+					for (int j = 0; j < dizi1_length; j++)
+					{
+						noktaTablosu[0, j + 2] = d_dizi1[j].ToString();
+						if (gap1 <= 0)
+							noktaTablosu[1, j + 2] = 0.ToString();
+						else
+							noktaTablosu[1, j + 2] = gap1.ToString();
+
+					}
+				}
+				/////////////////////////////////SAYILARI DİZDİM
+
+				for (int j = 0; j < dizi2_length; j++)
+				{
+					for (int i = 0; i < dizi1_length; i++)
+					{
+						if (noktaTablosu[j + 2, 0] == noktaTablosu[0, i + 2])
+						{
+
+							int v = Convert.ToInt32(noktaTablosu[j + 1, i + 2]);
+							int m = Convert.ToInt32(noktaTablosu[j + 2, i + 1]);
+							int n = Convert.ToInt32(noktaTablosu[j + 1, i + 1]);
+
+							int a = Math.Max(v + gap1, m + gap1);
+							int b = Math.Max(a, n + match1);
+							if (b < 0)
+								noktaTablosu[j + 2, i + 2] = 0.ToString();
+							else
+								noktaTablosu[j + 2, i + 2] = b.ToString();
+						}
+						else
+						{
+							int v = Convert.ToInt32(noktaTablosu[j + 1, i + 2]);
+							int m = Convert.ToInt32(noktaTablosu[j + 2, i + 1]);
+							int n = Convert.ToInt32(noktaTablosu[j + 1, i + 1]);
+
+							int a = Math.Max(v + gap1, m + gap1);
+							int b = Math.Max(a, n + mismatch1);
+							if (b < 0)
+								noktaTablosu[j + 2, i + 2] = 0.ToString();
+							else
+								noktaTablosu[j + 2, i + 2] = b.ToString();
+						}
+
+					}
+				}
+
+				///////////////PANELİ OLUŞTURDUM
+				for (int i = 0; i <= dizi2_length + 1; i++)
+				{
+					for (int j = 0; j <= dizi1_length + 1; j++)
+					{
+						Label lbl = new Label();
+						lbl.BorderStyle = BorderStyle.Fixed3D;
+						lbl.BackColor = Color.White;
+						lbl.Text = Convert.ToString(noktaTablosu[i, j]);
+						lbl.Size = new Size(25, 25);
+						lbl.Location = new Point(25 * j, 25 * i);
+						panel1.Controls.Add(lbl);
+
+					}
+				}
+
+				int z;
+
+				int max3 = Convert.ToInt32(noktaTablosu[2, 2]);
+				
+				for (z = 3; z <= dizi2_length + 1; z++)
+				{
+					
+					for (int r = 3; r < dizi1_length + 1; r++)
+					{
+						if (Convert.ToInt32(noktaTablosu[z, r]) > max3)
+						{
+							max3 = Convert.ToInt32(noktaTablosu[z, r]);
+							gonder(r);
+						}
+						else
+							continue;
+					}
+					
+				}
+			//	MessageBox.Show(max3.ToString());
+				
+				 void gonder(int r)
+				{
+					textBox3.Clear();
+					textBox4.Clear();
+					int x = z;
+					int y = r;
+					int max2 = 1;
+					int sayac1 = 0;
+					int sayac2 = 0;
+					int sayac3 = 0;
+					int sayac4 = 0;
+					int sayac5 = 0;
+					int sayac6 = 0;
+					int sayac8 = 0;
+
+
+					if (noktaTablosu[x, 0] == noktaTablosu[0, y])
+					{
+						sayac8++;
+						textBox3.SelectedText = noktaTablosu[x, 0];
+						textBox4.SelectedText = noktaTablosu[0, y];
+					}
+
+					while (max2 != 0)
+					{
+
+
+						if ((Convert.ToInt32(noktaTablosu[x, y - 1]) > Convert.ToInt32(noktaTablosu[x - 1, y])) && (Convert.ToInt32(noktaTablosu[x, y - 1]) > Convert.ToInt32(noktaTablosu[x - 1, y - 1])))
+						{
+							max2 = 0;
+							max2 = Convert.ToInt32(noktaTablosu[x, y - 1]);
+							if (noktaTablosu[x, 0] == noktaTablosu[0, y - 1])
+							{
+								sayac4++;
+								textBox3.SelectedText = noktaTablosu[x, 0];
+								textBox4.SelectedText = noktaTablosu[0,y-1];
+							}
+							else continue;
+							x = x;
+							y = y - 1;
+							sayac1 += max2;
+
+
+						}
+						if ((Convert.ToInt32(noktaTablosu[x - 1, y]) > Convert.ToInt32(noktaTablosu[x, y - 1])) && (Convert.ToInt32(noktaTablosu[x - 1, y]) > Convert.ToInt32(noktaTablosu[x - 1, y - 1])))
+						{
+							max2 = 0;
+							max2 = Convert.ToInt32(noktaTablosu[x - 1, y]);
+							if (noktaTablosu[x - 1, 0] == noktaTablosu[0, y])
+							{
+								sayac5++;
+								textBox3.SelectedText = noktaTablosu[x - 1, 0];
+								textBox4.SelectedText = noktaTablosu[0, y];
+							}
+							else continue;
+							x = x - 1;
+							y = y;
+							sayac2 += max2;
+
+						}
+						if ((Convert.ToInt32(noktaTablosu[x - 1, y - 1]) >= Convert.ToInt32(noktaTablosu[x, y - 1])) && (Convert.ToInt32(noktaTablosu[x - 1, y - 1]) >= Convert.ToInt32(noktaTablosu[x - 1, y])))
+						{
+							max2 = 0;
+							max2 = Convert.ToInt32(noktaTablosu[x - 1, y - 1]);
+
+							if (noktaTablosu[x - 1, 0] == noktaTablosu[0, y - 1] && noktaTablosu[0, y - 1] != noktaTablosu[0, y - 2])
+							{
+								sayac6++;
+								textBox3.SelectedText = noktaTablosu[x - 1, 0];
+								textBox4.SelectedText = noktaTablosu[0, y - 1];
+							}
+							else if (max2 == 0) continue;
+							else if (noktaTablosu[0, y - 1] == noktaTablosu[0, y - 2])
+							{
+								textBox3.SelectedText = noktaTablosu[x - 1, 0];
+								textBox4.SelectedText = "-";
+							}
+
+							x = x - 1;
+							y = y - 1;
+							sayac3 += max2;
+
+						}
+					}
+
+					string kelime = textBox3.Text;
+					string terskelime = "";
+					foreach (char harf in kelime)
+					{
+						terskelime = harf.ToString() + terskelime;
+					}
+					textBox3.Text = terskelime;
+
+					string kelime2 = textBox4.Text;
+					string terskelime2 = "";
+					foreach (char harf in kelime2)
+					{
+						terskelime2 = harf.ToString() + terskelime2;
+					}
+					textBox4.Text = terskelime2;
+
+				//	label4.Text = (sayac1 + sayac2 + sayac3).ToString();
+
+					label5.Text = (sayac6 + sayac8+sayac4+sayac5).ToString();
+
+				
+			}
+
+			}
+
+			}
+
+
+	}
+}
